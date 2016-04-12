7e1340cd6c49a268f1638c4f81faf5ee98999906
Martin Köhn <m.koehn@mip-consult.de>
2016-04-12 11:29:58 +0200

    add possibility to configure database connection

--- database.go
+++ database.go
@@ -1,17 +1,49 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
 
-// The MYSQL driver import
-import _ "github.com/go-sql-driver/mysql"
+	// The MYSQL driver import
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 var db *sql.DB
 
+// DBInformation describes the access data to the database
+type DBInformation struct {
+	DBName    string
+	User      string
+	Password  string
+	Host      string
+	Port      string
+	Collation string
+}
+
+// GetMysqlString returns the string to connect to a mysql database
+func (dbi *DBInformation) GetMysqlString() string {
+	if dbi.Host == "" {
+		dbi.Host = "localhost"
+	}
+	if dbi.Collation == "" {
+		dbi.Collation = "utf8_general_ci"
+	}
+	if dbi.DBName == "" {
+		panic("Error: Database name missing.")
+	}
+	if dbi.User == "" {
+		return "tcp(" + dbi.Host + ":" + dbi.Port + ")" + "/" + dbi.DBName + "?collation=" + dbi.Collation
+	}
+	return dbi.User + ":" + dbi.Password + "@tcp(" + dbi.Host + ":" + dbi.Port + ")" + "/" + dbi.DBName + "?collation=" + dbi.Collation
+}
+
 // GetDB returning current DB connection
-func GetDB() *sql.DB {
+func GetDB(dbi DBInformation) *sql.DB {
 	var err error
 	if db == nil {
-		db, err = sql.Open("mysql", "")
+		fmt.Println(dbi.GetMysqlString())
+		db, err = sql.Open("mysql", dbi.GetMysqlString())
 		if err != nil {
 			panic(err)
 		}
